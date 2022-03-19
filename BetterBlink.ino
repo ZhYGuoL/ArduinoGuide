e7ec1e3d3a6894e5c56726023554e12fa332e07f
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-03-19 00:56:31 -0700

    On/Off button

--- BetterBlink.ino
+++ BetterBlink.ino
@@ -1,3 +1,35 @@
+// check README.md to learn more about PWM (analog I/O)
+// I/O ports capable of PDM are marked with a "~" [~11, ~10, ~9, ~6, ~5, ~3]
+
+int button = 7; // change to " = 100" to remove off-button feature
+
+int inputPorts[] = {9};
+int outputPorts[] = {13};
+
+int inputPortsLength = sizeof(inputPorts) / sizeof(int);
+int outputPortsLength = sizeof(outputPorts) / sizeof(int);
+
 void setup() {
-    
+    pinMode(button, INPUT);
+
+
+    for (int i = 0; i < inputPortsLength; i++) {
+        pinMode(inputPorts[i], INPUT);
+    }
+
+    for (int i = 0; i < outputPortsLength; i++) {
+        pinMode(outputPorts[i], OUTPUT);
+    }
+
+}
+
+void loop() {
+    if (button != 100) {
+        int value = digitalRead(button);
+
+        for (int i = 0; i < outputPortsLength; i++)
+        {
+            pinMode(outputPorts[i], value);
+        }
+    }
 }
